5acc111c935e3155dfe7e620c36d88c20aab0c64
VinayPurohit31 <vinaypurohit31@gmail.com>
2025-02-12 15:40:40 +0530

    sign In screen work in progress

--- app/login/signin.jsx
+++ app/login/signin.jsx
@@ -1,12 +1,33 @@
 import { View, Text} from 'react-native'
 import React from 'react'
+import{StyleSheet} from 'react-native'
+import Colors from '../../constant/Colors'
 
 
 export default function SignInScreen() {
     return (
-        <View>
-            <Text>SignIn Screen</Text>
+        <View style={{
+            padding: 25,
+        }}>
+            <Text style={styles.testHeader}>Let's Sign You In</Text>
+            <Text style={styles.subHeader}>Welcome Back</Text>
+            <Text style={styles.subHeader}>We Missed You!</Text>
            
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    testHeader: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: 'black',
+    },
+    subHeader: {
+        marginTop: 10,
+        fontSize: 30,
+        fontWeight: 'bold',
+        color:Colors.GRAY,
+       
+    },
+})
